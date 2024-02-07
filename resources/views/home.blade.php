b306d7847ebedb78ce417e0a1c1254f8085b47fe
Isikawa Shotaro <zhengtailangs7@gmail.com>
2024-02-07 13:19:46 +0900

    chatgptでhome画面をtwitter風に。要検証

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,39 +3,45 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Timeline</h1>
-    @auth
-        <!-- ログインユーザーの場合 -->
-        <a href="{{ route('posts.create') }}">投稿する</a>
-        <a href="{{ route('profile.show', ['user' => Auth::id()]) }}">プロフィール</a>
-        <a href="{{ route('animals.edit', ['animal' => $user->animals->first()->id]) }}">animal</a>
-    @else
-        <!-- 未ログインユーザーの場合 -->
-        <a href="{{ route('login') }}">ログイン</a>
-        <a href="{{ route('register') }}">登録</a>
-    @endauth
-
-    <ul>
-        @foreach($posts as $post)
-            <li><a href="{{ route('posts.show', $post->id) }}">{{ $post->content }}</a></li>
-     
-            <div>
-                <!-- プロフィール画像 -->
-                @if ($post->user->profile_image_url)
-                    <img src="{{ $post->user->profile_image_url }} " alt="プロフィール画像">
-                @else
-                    <img src="{{ asset('images/1707116115.png') }}" alt="プロフィール画像" width="80px">
-                @endif
-
-                <!-- 動物の種族名 -->
-                @if ($post->animal)
-                    <span>{{ $post->animal->species }}</span>
-                @else
-                    <span>代替テキスト</span>
-                @endif
+    <div class="container">
+        <h1 class="mt-4 mb-4">Timeline</h1>
+        @auth
+            <!-- ログインユーザーの場合 -->
+            <div class="mb-4">
+                <a class="btn btn-primary me-2" href="{{ route('posts.create') }}">投稿する</a>
+                <a class="btn btn-secondary me-2" href="{{ route('profile.show', ['user' => Auth::id()]) }}">プロフィール</a>
+                <a class="btn btn-info" href="{{ route('animals.edit', ['animal' => $user->animals->first()->id]) }}">animal</a>
             </div>
-        @endforeach
+        @else
+            <!-- 未ログインユーザーの場合 -->
+            <div class="mb-4">
+                <a class="btn btn-primary me-2" href="{{ route('login') }}">ログイン</a>
+                <a class="btn btn-secondary" href="{{ route('register') }}">登録</a>
+            </div>
+        @endauth
 
-      
-    </ul>
+        <ul class="list-group">
+            @foreach($posts as $post)
+                <li class="list-group-item">
+                    <a href="{{ route('posts.show', $post->id) }}">{{ $post->content }}</a>
+                    
+                    <!-- プロフィール画像 -->
+                    <div class="mt-2">
+                        @if ($post->user->profile_image_url)
+                            <img src="{{ $post->user->profile_image_url }} " alt="プロフィール画像" class="rounded-circle me-2" width="40px">
+                        @else
+                            <img src="{{ asset('images/1707116115.png') }}" alt="プロフィール画像" class="rounded-circle me-2" width="40px">
+                        @endif
+                        
+                        <!-- 動物の種族名 -->
+                        @if ($post->animal)
+                            <span class="badge bg-primary">{{ $post->animal->species }}</span>
+                        @else
+                            <span class="badge bg-secondary">代替テキスト</span>
+                        @endif
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
